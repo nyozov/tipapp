31dd0c9689e1e9faf93e1e077d61c848dd202270
nikolay <n_yozov@hotmail.com>
2022-07-25 18:52:10 -0400

    jquery total calc

--- client.js
+++ client.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
- 
-  $('.tip-button').click(function(){
-   console.log(($(this).attr('value')))
-   console.log($('#amount').val())
-   $('#tip-calculated').text(($(this).attr('value')) * $('#amount').val())
- 
+$(document).ready(function () {
+  $(".tip-button").click(function () {
+    console.log($(this).attr("value"));
+    console.log($("#amount").val());
+    $("#tip-calculated").text($(this).attr("value") * $("#amount").val());
+  });
+
+  $("#people").on("input", function () {
+    $("#tip-total").text(
+      $(".tip-button").attr("value") * $("#amount").val() * $("#people").val()
+    );
   });
 });
